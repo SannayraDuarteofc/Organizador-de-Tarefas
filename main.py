b6cc97f7d0c4571c720f802b5413450ac40ad552
Sannayra <rodriguesduartesannayra@gmail.com>
2026-07-07 17:50:55 -0300

    Correção de erros e atualizações no código
    Adicionando a Verificação de entradas e removendo algumas repetições

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 def CadastroDeMaterias():
     Materias = {}
 
-    Materias["Materia"] = input('Matéria: ')
-    Materias["Prioridade"] = input('Nível de Prioridade (Alta, Média ou Baixa): ').lower()
-    Materias["Tempo"] = int(input('Tempo (em minutos): '))
+    Materia = input('Matéria: ').strip()
+    while True:
+        if Materia:
+            Materias["Materia"] = Materia
+            break
+        else:
+            Materia = input('Digite uma matéria válida: ').strip()
+
+    Prioridade = input('1. Alta'
+                       '\n2. Média\n'
+                       '3. Baixa\n'
+                       'Escolha o nível de prioridade: ')
+    while True:
+        if Prioridade.isnumeric():
+            if int(Prioridade) in (1, 2, 3):
+                Materias['Prioridade'] = int(Prioridade)
+                break
+
+        Prioridade = input('Digite um tipo de prioridade válida: ')
+
+    Tempo = input('Tempo: ')
+    while True:
+        if Tempo.isnumeric():
+            Tempo = int(Tempo)
+            if Tempo > 0:
+                Materias["Tempo"] = Tempo
+                break
+
+        Tempo = input('Digite uma quantidade de tempo válida: ')
 
     return Materias
 
@@ -26,18 +52,19 @@ def VisualizarMaterias(ListaMaterias):
         return
 
     for Materia in ListaMaterias:
-        if Materia["Prioridade"] == 'alta':
-            print(f'{cont}. {Materia["Materia"]} | Prioridade: {Materia["Prioridade"]} | Tempo: {Materia["Tempo"]//60:02d}:{Materia["Tempo"]%60:02d}')
+        if Materia["Prioridade"] == 1:
+            print(f'{cont}. {Materia["Materia"]} | Prioridade: Alta | Tempo: '
+                  f'{Materia["Tempo"]//60:02d}:{Materia["Tempo"]%60:02d}')
             cont += 1
 
     for Materia in ListaMaterias:
-        if Materia["Prioridade"] in ('media', 'média'):
-            print(f'{cont}. {Materia["Materia"]} | Prioridade: {Materia["Prioridade"]} | Tempo: {Materia["Tempo"]//60:02d}:{Materia["Tempo"]%60:02d}')
+        if Materia["Prioridade"] == 2:
+            print(f'{cont}. {Materia["Materia"]} | Prioridade: Média | Tempo: {Materia["Tempo"]//60:02d}:{Materia["Tempo"]%60:02d}')
             cont += 1
 
     for Materia in ListaMaterias:
-        if Materia["Prioridade"] == 'baixa':
-            print(f'{cont}. {Materia["Materia"]} | Prioridade: {Materia["Prioridade"]} | Tempo: {Materia["Tempo"]//60:02d}:{Materia["Tempo"]%60:02d}')
+        if Materia["Prioridade"] == 3:
+            print(f'{cont}. {Materia["Materia"]} | Prioridade: Baixa | Tempo: {Materia["Tempo"]//60:02d}:{Materia["Tempo"]%60:02d}')
             cont += 1
 
     print(f'Tempo Total: {TempoTotal(ListaMaterias)}\n')
@@ -45,12 +72,11 @@ def VisualizarMaterias(ListaMaterias):
 
 ListaMaterias = []
 
-print('1. Cadastrar Matéria\n'
-      '2. Visualizar Matérias de Hoje\n'
-      '3. Encerrar')
-
 while True:
-    Menu = int(input('O que deseja fazer: '))
+    Menu = int(input('1. Cadastrar Matéria\n'
+                     '2. Visualizar Matérias de Hoje\n'
+                     '3. Encerrar\n'
+                     'O que deseja fazer: '))
 
     if Menu == 1:
         Materia = CadastroDeMaterias()
@@ -61,7 +87,3 @@ while True:
         break
     else:
         print('Opção invalida! Tente novamente\n')
-
-    print('1. Cadastrar Matéria\n'
-          '2. Visualizar Matérias de Hoje\n'
-          '3. Encerrar')
