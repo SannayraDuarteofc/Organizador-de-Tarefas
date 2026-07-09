1f4598f00e8a48b5442b2f8de231a4b671d9f24d
Sannayra <rodriguesduartesannayra@gmail.com>
2026-07-09 17:02:59 -0300

    Otimizações no Código

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def CadastroDeMaterias():
         else:
             Materia = input('Digite uma matéria válida: ').strip()
 
-    Prioridade = input('1. Alta'
-                       '\n2. Média\n'
+    Prioridade = input('1. Alta\n'
+                       '2. Média\n'
                        '3. Baixa\n'
                        'Escolha o nível de prioridade: ')
     while True:
@@ -45,27 +45,23 @@ def TempoTotal(ListaMaterias):
     return f'{TotalHoras:02d}:{TotalMin:02d}'
 
 def VisualizarMaterias(ListaMaterias):
-    cont = 1
+    NiveisPrioridades = {
+        1: "Alta",
+        2: "Média",
+        3: "Baixa",
+    }
 
     if not ListaMaterias:
         print('Não há matérias para estudar hoje')
         return
+    for _ in range(len(ListaMaterias) - 1):
+        for j in range(len(ListaMaterias) - 1):
+            if ListaMaterias[j]["Prioridade"] > ListaMaterias[j+1]["Prioridade"]:
+                ListaMaterias[j], ListaMaterias[j+1] = ListaMaterias[j+1], ListaMaterias[j]
 
-    for Materia in ListaMaterias:
-        if Materia["Prioridade"] == 1:
-            print(f'{cont}. {Materia["Materia"]} | Prioridade: Alta | Tempo: '
+    for i, Materia in enumerate(ListaMaterias, start=1):
+            print(f'{i}. {Materia["Materia"]} | Prioridade: {NiveisPrioridades[Materia["Prioridade"]]}  | Tempo: '
                   f'{Materia["Tempo"]//60:02d}:{Materia["Tempo"]%60:02d}')
-            cont += 1
-
-    for Materia in ListaMaterias:
-        if Materia["Prioridade"] == 2:
-            print(f'{cont}. {Materia["Materia"]} | Prioridade: Média | Tempo: {Materia["Tempo"]//60:02d}:{Materia["Tempo"]%60:02d}')
-            cont += 1
-
-    for Materia in ListaMaterias:
-        if Materia["Prioridade"] == 3:
-            print(f'{cont}. {Materia["Materia"]} | Prioridade: Baixa | Tempo: {Materia["Tempo"]//60:02d}:{Materia["Tempo"]%60:02d}')
-            cont += 1
 
     print(f'Tempo Total: {TempoTotal(ListaMaterias)}\n')
 
@@ -73,7 +69,7 @@ def VisualizarMaterias(ListaMaterias):
 ListaMaterias = []
 
 while True:
-    Menu = int(input('1. Cadastrar Tarefas do dia'
+    Menu = int(input('1. Cadastrar Tarefas do dia\n'
                      '2. Cadastrar Matéria\n'
                      '3. Visualizar Matérias de Hoje\n'
                      '4. Remover Matérias\n'
