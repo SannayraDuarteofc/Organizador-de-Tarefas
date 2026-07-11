52767f34375e4e59d850bd8eea751dc9742ad710
Sannayra <rodriguesduartesannayra@gmail.com>
2026-07-11 00:04:13 -0300

    Novas Funcionalidades
    Cadastrar Tarefas do dia, Visualizar Matérias e Visualizar Afazeres de hoje

--- main.py
+++ main.py
@@ -4,38 +4,53 @@
 def CadastroDeTarefas(ListaMaterias):
     Tarefa = {}
 
-    Tipo = int(input('1. Estudos\n'
-                      '2. Outros\n'
-                      'Qual atividade deseja cadastrar hoje?'))
+    Tipo = input('1. Estudos\n'
+                '2. Outros\n'
+                'Qual atividade deseja cadastrar hoje?')
     while True:
-        if int(Tipo) == 1:
-            Tarefa["Tipo"] = 'Estudos'
-            Escolha = input('1. Escolher Materia\n'
-                                '2. Cadastrar Nova Matéria\n'
-                                ':')
-            while True:
-                if int(Escolha) == 1:
-                    for i,materia in enumerate(len(ListaMaterias), start= 1):
-                        print(f'{i}. {ListaMaterias[materia]}\n')
-                    IndiceMateria = int(input('Matéria: '))
+        if Tipo.isnumeric() and int(Tipo) in (1, 2):
+            if int(Tipo) == 1:
+                Tarefa["Tipo"] = 'Estudos'
+                Escolha = input('1. Escolher Materia\n'
+                                    '2. Cadastrar Nova Matéria\n'
+                                    ':')
+                while True:
+                    if int(Escolha) == 1:
+                        if not ListaMaterias:
+                            print('Não há matérias cadastradas')
+                            Op = input('Deseja cadastrar uma nova matéria?'
+                                       '1. Sim'
+                                       '2. Não'
+                                       ':')
+                            while True:
+                                if Op == 1:
 
-                elif int(Escolha) == 2:
-                    ListaMaterias.append(CadastroDeMaterias())
-                    continue
-                else:
-                    Escolha = input('Digite uma opção válida: ')
-                    continue
-                break
-            Tarefa["Materia"] = ListaMaterias[IndiceMateria - 1]
+                        for i, Materia in enumerate(ListaMaterias, start= 1):
+                            print(f'{i}. {Materia}\n')
+                        IndiceMateria = int(input('Matéria: '))
+                        if IndiceMateria >= 1 and IndiceMateria <= len(ListaMaterias):
+                            Tarefa["Materia"] = ListaMaterias[IndiceMateria - 1]
+                        else:
+                            Escolha = input('Digite uma opção válida: ')
+                            continue
 
-        elif Tipo == 2:
-            Tarefa["Tipo"] = 'Outros'
-            NovaTarefa = input('Tarefa:').strip()
-            while True:
-                if NovaTarefa:
-                    Tarefa["Afazer"] = NovaTarefa
+                    elif int(Escolha) == 2:
+                        NovaMateria = CadastroDeMaterias()
+                        ListaMaterias.append(NovaMateria)
+                        Tarefa["Materia"] = NovaMateria
+                    else:
+                        Escolha = input('Digite uma opção válida: ')
+                        continue
                     break
-                NovaTarefa = input('Digite uma tarefa válida: ').strip()
+
+            else:
+                Tarefa["Tipo"] = 'Outros'
+                NovaTarefa = input('Tarefa:').strip()
+                while True:
+                    if NovaTarefa:
+                        Tarefa["Afazer"] = NovaTarefa
+                        break
+                    NovaTarefa = input('Digite uma tarefa válida: ').strip()
         else:
             Tipo = input('Digite uma opção válida: ')
             continue
@@ -62,6 +77,8 @@ def CadastroDeTarefas(ListaMaterias):
                 break
 
         Tempo = input('Digite uma quantidade de tempo válida: ')
+
+    return Tarefa
 
 #Realiza o cadastro das matérias
 def CadastroDeMaterias():
@@ -95,11 +112,20 @@ def OrganizarTarefas(ListaTarefas):
 
     for _ in range(len(ListaTarefas) - 1):
         for j in range(len(ListaTarefas) - 1):
-            if ListaTarefas[j]["Prioridade"] > ListaMaterias[j+1]["Prioridade"]:
+            if ListaTarefas[j]["Prioridade"] > ListaTarefas[j+1]["Prioridade"]:
                 ListaTarefas[j], ListaTarefas[j+1] = ListaTarefas[j+1], ListaTarefas[j]
 
-#Imprime as tarefas do dia já ordenadas
-def VisualisarListaTarefas(ListaTarefas, ListaMaterias):
+#Mostra todas as matérias cadastradas, caso haja alguma
+def VisualizarMaterias(ListaMaterias):
+    if not ListaMaterias:
+        print('Não há matérias cadastradas')
+        return
+
+    for i, Materia in enumerate(ListaMaterias, start=1):
+        print(f'{i}. {Materia}')
+
+#Imprime as tarefas do dia já ordenadas, caso haja
+def VisualizarListaTarefas(ListaTarefas):
     NiveisPrioridades = {
         1: "Alta",
         2: "Média",
@@ -110,13 +136,15 @@ def VisualisarListaTarefas(ListaTarefas, ListaMaterias):
         print('Não há tarefas para hoje')
         return
 
+    OrganizarTarefas(ListaTarefas)
+
     for i, Tarefa in enumerate(ListaTarefas, start=1):
         if Tarefa["Tipo"] == 'Outros':
-            print(f'{i}. {Tarefa["Tipo"]} | Afazer: {Tarefa["Afazer"]} | Prioridade:'
+            print(f'{i}. Tipo: {Tarefa["Tipo"]} | Afazer: {Tarefa["Afazer"]} | Prioridade:'
                   f' {NiveisPrioridades[Tarefa["Prioridade"]]}  | '
                   f'Tempo: {Tarefa["Tempo"]//60:02d}:{Tarefa["Tempo"]%60:02d}')
         elif Tarefa["Tipo"] == 'Estudos':
-            print(f'{i}. {Tarefa["Tipo"]} | Matéria: {Tarefa["Estudos"]} | Prioridade:'
+            print(f'{i}. Tipo: {Tarefa["Tipo"]} | Matéria: {Tarefa["Materia"]} | Prioridade:'
                   f' {NiveisPrioridades[Tarefa["Prioridade"]]}  | '
                   f'Tempo: {Tarefa["Tempo"] // 60:02d}:{Tarefa["Tempo"] % 60:02d}')
 
@@ -136,15 +164,15 @@ while True:
                      'O que deseja fazer: '))
 
     if Menu == 1:
-
+        ListaTarefas.append(CadastroDeTarefas(ListaMaterias))
     elif Menu == 2:
-        VisuaListaTarefas(ListaTarefas)
+        VisualizarMaterias(ListaMaterias)
     elif Menu == 3:
-
+        VisualizarListaTarefas(ListaTarefas)
     elif Menu == 4:
-
+        RemoverTarefa(ListaTarefas)
     elif Menu == 5:
-
+        RemoverMateria(ListaMaterias)
     elif Menu == 6:
         break
     else:
